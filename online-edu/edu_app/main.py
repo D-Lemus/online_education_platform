1827e0b20afc54ae785aad1975ddff851b8e7118
Diego Lemus <diego.lemus@iteso.mx>
2025-11-25 14:55:54 -0600

    mongo conection uvicorn

---
+++ online-edu/edu_app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from edu_app.routes import users, courses, lessons
+
+app = FastAPI(
+    title="Online Education Platform",
+)
+
+app.include_router(users.router)
+app.include_router(courses.router)
+app.include_router(lessons.router)
